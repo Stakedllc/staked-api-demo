db97435398ab8ddcb5f737d048f99e4368175f30
Sam Mitchell <sammitchell650@gmail.com>
2019-08-05 21:44:32 -0700

    added chart to detail page

--- src/YieldsList.js
+++ src/YieldsList.js
@@ -19,32 +19,6 @@ import Divider from '@material-ui/core/Divider';
 import api from "./api.js";
 import {Line} from 'react-chartjs-2';
 
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
 
 const styles = theme => ({
   container: {
@@ -75,6 +49,9 @@ const styles = theme => ({
   },
   backNavLink: {
     cursor: "pointer"
+  },
+  breadcrumb: {
+    marginBottom: 10
   }
   
 });
@@ -185,12 +162,64 @@ class YieldsList extends React.Component {
       if(realYield < 0){
         realYieldLabelClassname = classes.redYieldLabel;
       }
+      const dates = selectedCurrencyTimeSeries.map(datum => datum.date.getMonth() + "/" + datum.date.getDate()).reverse();
+      const yields = selectedCurrencyTimeSeries.map(datum => Number(datum.yield) * 100);
+      const inflations = selectedCurrencyTimeSeries.map(datum => Number(datum.inflation_total) * 100);
+
+      const lineChartData = {
+        labels: dates,
+        datasets: [
+          {
+            label: 'Yield',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(76,175,80, 0.4)',
+            borderColor: 'rgba(76,175,80, 1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(76,175,80, 1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(76,175,80, 1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: yields
+          },
+          {
+            label: 'Inflation',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(244,67,54, 0.4)',
+            borderColor: 'rgba(244,67,54, 1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(244,67,54, 1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(244,67,54, 1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: inflations
+          }
+        ]
+      };
 
       return (
       <div className={classes.container}>
       <Breadcrumbs
           separator={<NavigateNextIcon fontSize="medium" />}
           aria-label="breadcrumb"
+          className={classes.breadcrumb} 
       >
 
         <Link className={classes.backNavLink} color="inherit" variant="h6" onClick={this.handleBackClick}>
@@ -201,7 +230,7 @@ class YieldsList extends React.Component {
           {selectedCurrency.currency}
         </Typography>
       </Breadcrumbs>
-      <Line data={data} />
+      <Line className={classes.lineChart} data={lineChartData} />
       <List className={classes.list}>
         <ListItem color="inherit" >
             <Typography color="textPrimary">Nominal Yield</Typography>
